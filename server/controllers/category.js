e7c52e81c25537a925c0e78f5e9e4375c59c964b
Ruma Das <rumaadl16@gmail.com>
2022-01-12 15:44:02 +1030

    adding category routes

---
+++ server/controllers/category.js
@@ -0,0 +1,22 @@
+const Category = require("../models/category");
+
+exports.create = async (req, res) => {
+  return;
+  console.log("");
+};
+exports.read = async (req, res) => {
+  return;
+  console.log("");
+};
+exports.update = async (req, res) => {
+  return;
+  console.log("");
+};
+exports.remove = async (req, res) => {
+  return;
+  console.log("");
+};
+exports.list = async (req, res) => {
+  return;
+  console.log("");
+};
