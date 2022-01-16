3b1ba959846632c395f1017a92e59771a7f55431
Ruma Das <rumaadl16@gmail.com>
2022-01-17 01:54:40 +1030

    adding different product page

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,27 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import AdminNav from "../../components/nav/AdminNav";
-import { getPruductsByCount } from "../../functions/product";
-import AdminProductCard from "../../components/cards/AdminProductCard";
 
 const AdminDashboard = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    loadAllProducts();
-  }, []);
-
-  const loadAllProducts = () => {
-    getPruductsByCount(100)
-      .then((res) => {
-        setProducts(res.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        setLoading(false);
-        console.log(err);
-      });
-  };
   return (
     <div className="containe-fluid">
       <div className="row">
@@ -29,22 +9,7 @@ const AdminDashboard = () => {
           <AdminNav />
         </div>
         <div className="col">
-          {loading ? (
-            <h4 className="text-danger">Loading ...</h4>
-          ) : (
-            <h3>Product List</h3>
-          )}
-          <div className="col">
-            <div className="row">
-              {products.map((product) => {
-                return (
-                  <div className="col-md-4" key={product._id}>
-                    <AdminProductCard product={product} />
-                  </div>
-                );
-              })}
-            </div>
-          </div>
+          <h3>Admin Dashboard</h3>
         </div>
       </div>
     </div>
