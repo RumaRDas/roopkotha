93814a081539e7a58d6970c3e89f3c5dada5a779
Ruma Das <rumaadl16@gmail.com>
2022-01-17 15:10:16 +1030

    successfuly apply to get product data in update form

---
+++ client/src/components/forms/ProductUpdateForm.js
@@ -0,0 +1,125 @@
+import React from "react";
+
+import { Select } from "antd";
+const { Option } = Select;
+
+const ProductUpdateForm = ({
+  handleSubmit,
+  handleChange,
+  values,
+  setValues,
+}) => {
+  //Destructure useState values
+  const {
+    title,
+    description,
+    price,
+    quantity,
+    categories,
+    category,
+    subcates,
+    subcate,
+    images,
+    colors,
+    types,
+    color,
+    type,
+  } = values;
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <lable>Title</lable>
+          <input
+            type="text"
+            name="title"
+            className="form-control"
+            value={title}
+            onChange={handleChange}
+          />
+          <p className="text-danger">Title need More then 3 Character</p>
+        </div>
+        <div className="form-group">
+          <lable>Description</lable>
+          <textarea
+            type="text"
+            name="description"
+            className="form-control"
+            value={description}
+            onChange={handleChange}
+          />
+          <div className="form-group">
+            <lable>Price</lable>
+            <input
+              type="number"
+              name="price"
+              className="form-control"
+              value={price}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <lable>Shipping</lable>
+            <select
+              name="shipping"
+              className="form-control"
+              onChange={handleChange}
+            >
+              <option>Plese Select</option>
+              <option value="No">No</option>
+              <option value="Yes">Yes</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <lable>Quantity</lable>
+            <input
+              type="number"
+              name="quantity"
+              className="form-control"
+              value={quantity}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <lable>Color</lable>
+            <select
+              name="color"
+              className="form-control"
+              onChange={handleChange}
+            >
+              <option>Plese Select</option>
+              {colors.map((c) => (
+                <option key={c} value={c}>
+                  {c}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="form-group">
+            <lable>Type</lable>
+            <select
+              name="type"
+              className="form-control"
+              onChange={handleChange}
+            >
+              <option>Plese Select</option>
+              {types.map((t) => (
+                <option key={t} value={t}>
+                  {t}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+
+        {/* Ant design select option */}
+
+        {/* ____ */}
+        {/* {subOptions ? subOptions.length : "no subs yet"} */}
+        <br />
+        <button className="btn btn-info btn-block">Save</button>
+      </form>
+    </div>
+  );
+};
+export default ProductUpdateForm;
--- client/src/pages/admin/product/ProductUpdate.js
+++ client/src/pages/admin/product/ProductUpdate.js
@@ -2,17 +2,82 @@ import React, { useState, useEffect } from "react";
 import AdminNav from "../../../components/nav/AdminNav";
 import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
-import { createProduct } from "../../../functions/product";
 import { getCategories, getCategorySubs } from "../../../functions/category";
 import { getSubCates } from "../../../functions/subcate.js";
 import { Link } from "react-router-dom";
-import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
-import ProductCreateForm from "../../../components/forms/ProductCreateForm";
 import FileUpload from "../../../components/forms/FileUpload";
 import { LoadingOutlined } from "@ant-design/icons";
+import ProductUpdateForm from "../../../components/forms/ProductUpdateForm";
 
-const ProductUpdate = () => {
+import { getPruduct } from "../../../functions/product";
+
+// import { useParams } from "react-router-dom";
+
+const initialState = {
+  title: "",
+  description: "",
+  price: "",
+  categories: [],
+  category: "",
+  subcates: [],
+  subcate: [],
+  quantity: "",
+  images: [],
+  //  shipping: ["Yes", "No"],
+  colors: [
+    "White",
+    "Black",
+    "Red",
+    "Pink",
+    "Blue",
+    "Green",
+    "Yellow",
+    "Others",
+  ],
+  types: [
+    "Cotton",
+    "Muslin",
+    "Silk",
+    "Katan",
+    "Gorgette",
+    "Organza",
+    "Casula",
+    "PartyWare",
+    "Others",
+  ],
+  color: "",
+  type: "",
+};
+const ProductUpdate = ({ match }) => {
+  //state for all values from initialState
+  const [values, setValues] = useState(initialState);
   const { user } = useSelector((state) => ({ ...state }));
+  const { slug } = match.params;
+  //router
+  // let params = useParams();
+
+  useEffect(() => {
+    loadProduct();
+  }, []);
+
+  const loadProduct = () => {
+    getPruduct(slug)
+      .then((p) => {
+        setValues({ ...values, ...p.data });
+        // console.log("SINGLE PRODUCT", p);
+      })
+      .catch((err) => {});
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //
+  };
+  //for updateing form event change on change
+  const handleChange = (e) => {
+    //  setValues({ ...values, [e.target.name]: e.target.values });
+    // console.log(e.target.name, "----------", e.target.value);
+  };
 
   return (
     <div className="container-fluid">
@@ -20,9 +85,16 @@ const ProductUpdate = () => {
         <div className="col-md-3">
           <AdminNav />
         </div>
-
+        {/* {JSON.stringify(match.params.slug)} */}
+        {/* {JSON.stringify(values)} */}
         <div className="col-md-9">
           <h3>Product UpDate Form</h3>
+          <ProductUpdateForm
+            handleSubmit={handleSubmit}
+            handleChange={handleChange}
+            values={values}
+            setValues={setValues}
+          />
           <hr />
         </div>
       </div>
