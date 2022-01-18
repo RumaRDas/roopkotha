e3e28b398b53e602aa9d4c2cf7f736d19c72a73e
Ruma Das <rumaadl16@gmail.com>
2022-01-18 21:21:55 +1030

    adding best seller an latest product in home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,47 +1,22 @@
-import React, { useEffect, useState } from "react";
-import { getProductsByCount } from "../functions/product";
-import ProductCard from "../components/cards/ProductCard";
+import React from "react";
 import Jumbotron from "../components/cards/Jumbotron";
-import LoadingCard from "../components/cards/LoadingCard";
+import Newarrivals from "../components/home/Newarrivals";
+import BestSold from "../components/home/BestSold";
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    loadAllProducts();
-  }, []);
-
-  const loadAllProducts = () => {
-    setLoading(true);
-    getProductsByCount(3)
-      .then((res) => {
-        setProducts(res.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        setLoading(false);
-        console.log(err);
-      });
-  };
   return (
     <>
       <div className="jumbotron text-info h1 font-weight-bold text-center">
         <Jumbotron text={["New Arrivals", "Latest Products", "Best Sellers"]} />
       </div>
-      <div className="container">
-        {loading ? (
-          <LoadingCard count={3} />
-        ) : (
-          <div className="row">
-            {products.map((product) => (
-              <div className="col-md-4" key={product._id}>
-                <ProductCard product={product} />
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
+      <h3 className="text-center p-3 mb-5  mt-5 display-4 jumbotron text-danger">
+        New Arrivals
+      </h3>
+      <Newarrivals />
+      <h3 className="text-center p-3 mb-5  mt-5 display-4 jumbotron text-danger">
+        Best Sold
+      </h3>
+      <BestSold />
     </>
   );
 };
