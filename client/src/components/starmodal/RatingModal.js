a88501c20d73ce4407cea2b8e54e9806476b8bdd
Ruma Das <rumaadl16@gmail.com>
2022-01-20 01:52:22 +1030

    adding redereck user to same product page after login

--- client/src/components/starmodal/RatingModal.js
+++ client/src/components/starmodal/RatingModal.js
@@ -3,14 +3,32 @@ import { Modal, Button } from "antd";
 import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
 import { StarOutlined } from "@ant-design/icons";
+import { useHistory, useParams } from "react-router-dom";
 
 const RatingModal = ({ children }) => {
   const [modalVisible, setModalVisible] = useState(false);
 
   const { user } = useSelector((state) => ({ ...state }));
+  // this component is not use app js so we need to use react-router-dom for import  useHistory
+  let history = useHistory();
+  //
+  let { slug } = useParams();
+  console.log("SLUG :", slug);
+
+  const handleModal = () => {
+    if (user && user.token) {
+      setModalVisible(true);
+    } else {
+      history.push({
+        pathname: "/login",
+        state: { from: `/product/${slug}` },
+      });
+    }
+  };
+
   return (
     <>
-      <div onClick={() => setModalVisible(true)}>
+      <div onClick={handleModal}>
         <StarOutlined className="text-danger" />
         <br />
         {user ? "Leave rating" : " Loging to leave rating"}
