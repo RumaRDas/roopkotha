71dfaef3b83f8afd4cc7863938ff4656e4722422
Ruma Das <rumaadl16@gmail.com>
2022-01-23 23:40:22 +1030

    adding product display in related categories

--- client/src/pages/Category/CategoryHome.js
+++ client/src/pages/Category/CategoryHome.js
@@ -1,13 +1,50 @@
 import React, { useEffect, useState } from "react";
-import { getCategories } from "../../functions/category";
+import { getCategory } from "../../functions/category";
 import { Link } from "react-router-dom";
 import ProductCard from "../../components/cards/ProductCard";
 
 const CategoryHome = ({ match }) => {
-  useEffect(() => {}, []);
+  const [category, setCategory] = useState({});
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const { slug } = match.params;
+
+  useEffect(() => {
+    setLoading(true);
+    getCategory(slug).then((res) => {
+      console.log(JSON.stringify(res.data, null, 4));
+      setCategory(res.data.category);
+      setProducts(res.data.products);
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <div className="container">
-      <p>{match.params.slug}</p>
+      <div className="row">
+        <div className="col">
+          {loading ? (
+            <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+              Loading....
+            </h4>
+          ) : (
+            <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+              {products.length} Products in
+              <span className="text-info"> {category.name}"</span> category
+            </h4>
+          )}
+        </div>
+      </div>
+      <div className="row">
+        {products.map((p) => {
+          return (
+            <div className="col-md-4 pb-5" key={p._id}>
+              <ProductCard product={p} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
