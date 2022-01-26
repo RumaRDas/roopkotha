b12e757b7147b4d2020f0a21f77eb4b5ed4488d2
Ruma Das <rumaadl16@gmail.com>
2022-01-26 15:28:41 +1030

    adding images with reactmodal in cart page

--- client/src/components/cards/ProductCartInCheckout.js
+++ client/src/components/cards/ProductCartInCheckout.js
@@ -1,10 +1,20 @@
 import React from "react";
+import ModalImage from "react-modal-image";
+import laptop from "../../images/laptop.jpg";
 
 const ProductCartInCheckout = ({ p }) => {
   return (
     <tbody>
       <tr>
-        <td>Image</td>
+        <td>
+          <div>
+            {p.images.length ? (
+              <ModalImage small={p.images[0].url} large={p.images[0].url} />
+            ) : (
+              <ModalImage small={laptop} large={laptop} />
+            )}
+          </div>
+        </td>
         <td>{p.title}</td>
         <td>$ {p.price}</td>
         <td>{p.type}</td>
